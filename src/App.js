982f2ff977e3d63e40eb4e1a5fdb5a09feb65890
chaerin <achrvv@sookmyung.ac.kr>
2021-05-15 17:10:46 +0900

    routing & module 설치

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Main from "./pages/Main";
+import Host from "./pages/Host";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <Router>
+      <Switch>
+        <Route path="/" exact>
+          <Main />
+        </Route>
+        <Route path="/host">
+          <Host />
+        </Route>
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
