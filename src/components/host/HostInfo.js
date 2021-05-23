47158e8a33ec26b5ec8c0db6cb11ea96f2a0f7ac
chaerin <achrvv@sookmyung.ac.kr>
2021-05-23 21:37:53 +0900

    feat: HostInfo 컨포넌트 생성

---
+++ src/components/host/HostInfo.js
@@ -0,0 +1,16 @@
+import React from "react";
+import styled from "styled-components";
+
+const HostInfoWrap = styled.div`
+  width: 120rem;
+`;
+
+const HostInfo = () => {
+  return (
+    <HostInfoWrap>
+      <div></div>
+    </HostInfoWrap>
+  );
+};
+
+export default HostInfo;
