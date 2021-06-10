bc53322b2908e73719b1eb22949ffcdfad6ca993
chaerin <achrvv@sookmyung.ac.kr>
2021-06-10 17:52:36 +0900

    feat: Text Component 분리

---
+++ src/components/host/TextMobile.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const TextMobile = () => {
+  return <div>Mobile</div>;
+};
+
+export default TextMobile;
--- src/pages/Host.js
+++ src/pages/Host.js
@@ -1,5 +1,6 @@
 import React from "react";
 import styled from "styled-components";
+import { useMediaQuery } from "react-responsive";
 import HeaderImg from "../assets/img/img_main_01.webp";
 import symbolImg from "../assets/img/ic_symbol_soptbnb.svg";
 import logoImg from "../assets/img/ic_soptbnb.svg";
@@ -8,6 +9,10 @@ import HeaderImg2 from "../assets/img/img_main_02.webp";
 import Footer from "../components/common/Footer";
 import HostInfo from "../components/host/HostInfo";
 import ImageSlider from "../components/host/ImageSlider";
+import TextPC from "../components/host/TextPC";
+import TextMobile from "../components/host/TextMobile";
+import SupportTextMobile from "../components/host/SupportTextMobile";
+import SupportTextPC from "../components/host/SupportTextPC";
 
 const Header = styled.div`
   width: 120rem;
@@ -85,82 +90,6 @@ const Header = styled.div`
       position: absolute;
       top: 48.8rem;
       left: 2.4rem;
-    }
-  }
-`;
-const TextfieldWrap = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  .mainTitle {
-    margin-left: 5.7rem;
-    margin-top: 8.3rem;
-    font-weight: 600;
-    font-size: 3.1rem;
-    line-height: 4.5rem;
-    color: #373737;
-  }
-  .text {
-    padding-top: 8.7rem;
-    padding-left: 14.3rem;
-    width: 23rem;
-    &__top {
-      font-weight: 600;
-      font-size: 1.7rem;
-      line-height: 2.3rem;
-
-      color: #373737;
-    }
-    &__middle {
-      margin-top: 1.9rem;
-      font-weight: 500;
-      font-size: 1.2rem;
-      line-height: 165%;
-      color: #747474;
-    }
-    &__bottom {
-      margin-top: 1.9rem;
-      font-weight: 500;
-      font-size: 1rem;
-      line-height: 150%;
-      color: #373737;
-      text-decoration: underline;
-      /* border-bottom: 1px solid #373737; */
-    }
-  }
-  .supportTitle {
-    width: 108.6rem;
-    font-weight: 600;
-    font-size: 2.9rem;
-    line-height: 3.9rem;
-    letter-spacing: -0.03em;
-    color: #373737;
-  }
-  .supportInfo {
-    display: flex;
-    flex-direction: row;
-  }
-  @media screen and (max-width: 500px) {
-    display: flex;
-    justify-content: start;
-    align-items: flex-start;
-    flex-direction: column;
-    width: 100vw;
-    .mainTitle {
-      margin-left: 2.4rem;
-      margin-top: 3.1rem;
-    }
-    .text {
-      padding-top: 4rem;
-      padding-left: 2.4rem;
-      width: 23rem;
-    }
-    .supportTitle {
-      width: 100vw;
-    }
-    .supportInfo {
-      display: flex;
-      flex-direction: column;
     }
   }
 `;
@@ -268,6 +197,7 @@ const ImgSliderWrap = styled.div`
   }
 `;
 function Host() {
+  const isMobile = useMediaQuery({ maxWidth: 500 });
   return (
     <>
       <Header bg={HeaderImg}>
@@ -285,28 +215,7 @@ function Host() {
         </div>
         <div className="startBtn">시작하기</div>
       </Header>
-      <TextfieldWrap>
-        <div className="mainTitle">
-          내 인생의 새로운 시작, <br />
-          호스트가 되면 가능합니다.
-        </div>
-        <div className="text">
-          <div className="text__top">새로운 가능성이 열립니다</div>
-          <div className="text__middle">
-            호스팅을 통해 내가 원하는 시간에 유연하게 일하면서 부수입을 올리고
-            전 세계 사람들을 만나세요.
-          </div>
-          <div className="text__bottom">호스팅에 대해 알아보기</div>
-        </div>
-        <div className="text">
-          <div className="text__top">안심할 수 있는 호스팅</div>
-          <div className="text__middle">
-            연중무휴 지원, 호스트 커뮤니티, 호스트를 위한 맞춤 도구와 데이터
-            분석, 교육 등 에어비앤비는 다방면으로 호스트의 성공을 지원합니다.
-          </div>
-          <div className="text__bottom">에어비앤비의 호스트 지원 방법</div>
-        </div>
-      </TextfieldWrap>
+      {isMobile ? <TextMobile /> : <TextPC />}
       <ReviewCard>
         <img className="reviewImg" src={reviewCardImg} alt="" />
         <div className="reviewText">
@@ -325,61 +234,7 @@ function Host() {
         <div className="title">호스팅에 대해 알아보기</div>
         <ImageSlider />
       </ImgSliderWrap>
-      <TextfieldWrap style={{ flexDirection: "column" }}>
-        <div className="supportTitle">애어비앤비의 호스트 지원</div>
-        <div className="supportInfo">
-          <div
-            className="text"
-            // style={{
-            //   paddingLeft: 0,
-            //   paddingTop: "2.4rem",
-            //   width: "27.2rem"
-            // }}
-          >
-            <div className="text__top">호스트를 보호하는 다양한 프로그램</div>
-            <div className="text__middle">
-              혹시라도 사고가 발생하는 경우에 대비하여, 대부분의 에어비앤비
-              예약에는 미화 최대 1백만 달러의 재산 피해 보호 및 책임배상보험이
-              포함됩니다.
-            </div>
-            <div className="text__bottom">에어비앤비의 호스트 보호 장치</div>
-          </div>
-          <div
-            className="text"
-            // style={{
-            //   paddingLeft: "13.5rem",
-            //   paddingTop: "2.4rem",
-            //   width: "27.2rem"
-            // }}
-          >
-            <div className="text__top">코로나19 방역을 위한 안전 수칙</div>
-            <div className="text__middle">
-              커뮤니티의 건강과 안전을 위해 전문가와 협력하여 방역 수칙과
-              호스트에게 도움이 될 청소 절차를 마련했습니다.
-            </div>
-            <div className="text__bottom">강화된 청소 절차 알아보기</div>
-          </div>
-          <div
-            className="text"
-            // style={{
-            //   paddingLeft: "13.5rem",
-            //   paddingTop: "2.4rem",
-            //   width: "27.2rem"
-            // }}
-          >
-            <div className="text__top">높은 게스트 기준</div>
-            <div className="text__middle">
-              호스트는 예약 확정 전에 게스트 신원과 게스트에 대한 후기를 확인할
-              수 있으므로 안심하고 예약을 받을 수 있습니다. 에어비앤비는 새로운
-              게스트 기준 정책을 통해 게스트가 지켜야 할 사항을 명확히 규정하고
-              있습니다.
-            </div>
-            <div className="text__bottom">
-              안심 호스팅을 위한 에어비앤비의 정책과 기능
-            </div>
-          </div>
-        </div>
-      </TextfieldWrap>
+      {isMobile ? <SupportTextMobile /> : <SupportTextPC />}
       <Header style={{ marginTop: "4.5rem" }} bg={HeaderImg2}>
         <div className="text">
           <div className="text__middle">호스팅 시작하기</div>
